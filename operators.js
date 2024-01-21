f2108003babeb9417c020d0211444e6327f9f6f0
rithika21 <rithikakv21@gmail.com>
2024-01-21 18:54:16 +0530

    javascript

---
+++ operators.js
@@ -0,0 +1,53 @@
+//BASIC PRINTING IN JAVASCRIPT:
+a=10
+console.log(a)
+console.log("hello !!! welcome rithika.....")
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//VARIABLES
+a='a'
+console.log("This variable belongs to gendral variable value:",a)
+var b='var'
+console.log("This variable belongs to var type value:",b)
+let c='let'
+console.log("This variable belongs to let type value:",c)
+const d=100
+console.log("This variable belongs to const type value:",d)
+d=200
+console.log("This variable belongs to const type value:",d)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//OPERATORS TYPES :
+//assignment operator
+var a=10
+console.log("assignment operator :",a)
+
+
+//arithmetic operators---> +,-,*,/,%
+var aA=10
+var bA=10
+console.log(typeof aA)
+console.log("Arithmetic operators addition o/p :",aA+bA)
+console.log("Arithmetic operators subtraction o/p :",aA-bA)
+console.log("Arithmetic operators multiplicatio o/p :",aA*bA)
+console.log("Arithmetic operators division o/p :",aA/bA)
+console.log("Arithmetic operators modulus o/p :",aA%bA)
+
+
+//LOGICAL OPERATORS:--->&& ,||,!
+var aL=10
+var bL=10
+console.log("LOGICAL OPERATORS && o/p:",aL&&bL)
+console.log("LOGICAL OPERATORS || o/p:",aL||bL)
+console.log("LOGICAL OPERATORS ! o/p:",!aL)
+console.log(10||20)
+console.log(0||20)
+console.log(20||0)
+console.log(100||20)
+console.log(100||20&&0)
+console.log((100||20)&&0)
+console.log((100||20)&&(0))
+console.log(0&&(10||20))
+
+
+
